af45508abb86d5df39f88e97bcd570fac2839476
Agus Rizal <57043034+agusrizal17@users.noreply.github.com>
2024-07-20 20:44:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ let sender;
 if (url.searchParams.get('by') != null) {
   sender = url.searchParams.get('by');
 } else {
-  sender = "MFarelS";
+  sender = "Rizal";
 }
 
 
 document.querySelector(".tombol").addEventListener('click', function () {
-  Swal.fire("Hallo Pacarku", "Aku ada pertanyaan nih buat kamu yangg", "question").then(function () {
+  Swal.fire("Hallo Kamu", "Aku ada pertanyaan nih buat kamu ", "question").then(function () {
     Swal.fire("Jawab yang jujur ya!").then(function () {
       Swal.fire("Awas aja kalau kamu bohong", "", "error").then(function () {
 
@@ -64,7 +64,7 @@ document.querySelector(".tombol").addEventListener('click', function () {
                       /* Read more about isConfirmed, isDenied below */
                       if (result.isConfirmed) {
                         Swal.fire(`Huhu iya ${sender} juga kangen ${nama} , makasihhh yaa`).then(function () {
-                          Swal.fire('Terakhir deh sayang').then(function () {
+                          Swal.fire('Terakhir deh ini').then(function () {
                             Swal.fire('Coba klik ikon hati di paling bawah dong')
                           })
                         })
